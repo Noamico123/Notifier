36fe432b3213eae4372c386654042e17f0ea1061
Noamico123 <noamico123@gmail.com>
2023-03-08 12:54:26 +0200

    added base files

---
+++ consts.py
@@ -0,0 +1,19 @@
+
+
+EVENT = 'Event'
+COMPANY = 'Company'
+WEBINAR = 'Webinar'
+CONTENT_ITEM = 'ContentItem'
+COMPANY_FOR_EVENT = 'CompanyForEvent'
+COMPANY_FOR_WEBINAR = 'CompanyForWebinar'
+COMPANY_COMPETITOR = 'CompanyCompetitor'
+
+
+NOTIFIER_HEADER = ':: Notifier ::'
+NOTIFY_MSG = r'Object Type: {object_type} Description: {reason}'
+
+NEW_OBJ_REASON = 'New object'
+PHYSICALLY_DELETE_REASON = 'Object is now physically deleted'
+IS_DELETED_REASON = 'is_deleted attribute has changed'
+IS_BLACKLIST_REASON = 'is_blacklisted attribute has changed'
+CRAWLING_STATUS_REASON = 'crawling_status attribute has changed and is now one of TEXT_ANALYZED, TEXT_UPLOADED'
